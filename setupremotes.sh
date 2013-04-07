41d9871e9b9ec7dc7e0c26d44775c5175e0de597
riking <rikingcoding@gmail.com>
2013-04-06 20:18:24 -0700

    Create setupremotes.sh

---
+++ setupremotes.sh
@@ -0,0 +1,12 @@
+cd Bukkit
+git remote add release git://github.com/Bukkit/Bukkit.git
+git remote add riking git@github.com:riking/Bukkit.git
+git remote add origin git://github.com/riking/Bukkit.git
+cd ..
+
+cd CraftBukkit
+git remote add release git://github.com/Bukkit/CraftBukkit.git
+git remote add riking git@github.com:riking/CraftBukkit.git
+git remote add origin git://github.com/riking/CraftBukkit.git
+cd ..
+
